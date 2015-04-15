f212d3aa951a7b9515f7b4e490f7fd86fff9ad7e
rslechta <slec0016@stthomas.edu>
2015-04-14 19:49:35 -0500

    Update ID.java

--- src/ID.java
+++ src/ID.java
@@ -1,232 +1,210 @@
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 
 public class ID {
-	/**
-	 * 
-	 * Miguel Velez
-	 * March 7, 2015
-	 * 
-	 * This class TODO
-	 * 
-	 * Class variables:
-	 * 		static int idLengthInBytes
-	 * 			The length in bytes of the ID.
-	 * 		
-	 * 		static LinkedListQueue idQueue
-	 * 			The ID queue.
-	 * 
-	 * 		static int maxQueueLength
-	 * 			The maximum length of the queue.
-	 * 
-	 * 		static int queueLength
-	 * 			The current queue length.
-	 * 
-	 *  	static SecureRandom secureRandom
-	 *  		TODO
-	 *  
-	 *   	static ID zeroID
-	 *   		TODO
-	 *   	
-	 *   	byte[] id
-	 *   		TODO
-	 *  
-	 * Constructors:
-	 * 		private ID()
-	 * 			TODO
-	 * 	
-	 * 		public ID(byte[] byteArray)
-	 * 			TODO 
-	 * 
-	 * Methods:
-	 *		public static ID idFactory() 
-	 *			TODO
-	 *
-	 *		private static ID createZeroID()
-	 *			TODO
-	 *
-	 *		public static void generateID() 
-	 *			TODO
-	 *
-	 *		public static int getIDLength()
-	 *			TODO
-	 *
-	 *		private static LinkedListQueue getQueue()
-	 *			Return the id queue.
-	 *
-	 *		public static int getMaxQueueLength()
-	 *			Return the max queue length.
-	 *
-	 * 		public static int getQueueLength()
-	 * 			Return the queue length.
-	 * 
-	 * 		public static ID getZeroID()
-	 * 			Return the zero ID.
-	 * 
-	 * 		public static void setIDLength(int lengthInByte)
-	 * 			Sets the id length.
-	 * 		
-	 * 		public static void setMaxQueueLength(int length)
-	 * 			Sets the max queue length.
-	 * 
-	 * 		public byte[] getBytes()
-	 * 			TODO
-	 * 
-	 *  	public boolean equals(Object other)
-	 *  		TODO
-	 *  
-	 *  	public int hashCode()
-	 *  		TODO
-	 *  
-	 *   	public String toString()
-	 *   		TODO
-	 *       
-	 * Modification History:
-	 * 		March 7, 2015
-	 * 			Original version
-	 *  
-	 */
-	
-	private static int 				idLengthInBytes = 16;
-	private static LinkedListQueue 	idQueue = new LinkedListQueue();
-	private static int				maxQueueLength = 0; // TODO length?
-	private static int				queueLength = 0; // TODO min
-	private static SecureRandom		secureRandom = new SecureRandom(); // TODO what is this?
-	private static ID				zeroID; // TODO how to initialize
+
+	private static int idLengthInBytes = 16;
+	private static LinkedList<ID> idQueue = new LinkedList<ID>();
+	private static int maxQueueLength = 50;
+	private static int queueLength  = 0;
+	private static SecureRandom secureRandom = new SecureRandom();
+	private static ID zeroID = ID.createZeroID();
 	
 	private byte[] id;
 	
 	private ID()
 	{
-		//TODO
+		this.id = new byte[ID.getIDLength()];
+		ID.secureRandom.nextBytes(this.id);
 	}
 	
-	public ID(byte[] byteArray) 
+	/**
+	 * Constructs a new ID by using the input byte array as the ID.
+	 * @param byteArray
+	 */
+	
+	public ID (byte[] byteArray)
 	{
-		// Check if byte array is null
-		if(byteArray == null) {
-			// TODO exception or warning message?
+		if(byteArray.length < idLengthInBytes)
+		{
+			throw new IllegalArgumentException("Byte array has to be exactly "+ID.idLengthInBytes+ " bytes long. Shame.");
 		}
-		
-		// Check if the byte array length is the same as the length in bytes of the id
-		if (byteArray.length != ID.idLengthInBytes) {
-			// TODO exception or warning message?
-		}
-		
-		// TODO
 		this.id = byteArray.clone();
-		
 	}
 	
-	public static ID idFactory() 
+	/**
+	 * Checks if the queue length is equal to 0. If so, returns an ID from the queue. If not, constructs a new ID.
+	 * @return 
+	 * 		A return ID.
+	 */
+	public synchronized static ID idFactory()
 	{
-		// TODO
-		return null;	
+		ID returnID;
+		if(ID.queueLength==0)
+		{
+			returnID = new ID();
+		}else{
+			returnID = (ID)ID.getQueue().remove();
+			ID.queueLength = ID.queueLength - 1;
+		}
+		
+		return returnID;
 	}
 	
+	/**
+	 * Creates a ID consisting entirely of zeros. 
+	 * @return
+	 * 		An id consisting entirely of zeros. 
+	 */
 	private static ID createZeroID()
 	{
-		// TODO
-		return null;
-	}
-	
-	public static void generateID() 
-	{
-		// TODO
-		if(ID.queueLength < ID.maxQueueLength) 
+		byte[] zeroByteArray = new byte[idLengthInBytes];
+		for(int i = 0; i< idLengthInBytes;i++)
 		{
-			// TODO
+			zeroByteArray[i] = 0;
 		}
+		
+		return new ID(zeroByteArray);
 	}
 	
-	public static int getIDLength()
+	/**
+	 * Fills the queue to the amount specified by the maxQueueLength static parameter.
+	 */
+	public static synchronized void generateID()
 	{
-		// TODO
-		return ID.idLengthInBytes;
+		while(ID.queueLength<ID.maxQueueLength)
+		{
+			idQueue.add(new ID());
+			ID.queueLength++;
+		}//Add new IDs to the queue until we are at maximum.
 	}
 	
-	private static LinkedListQueue getQueue()
+	/**
+	 * Basic getter.
+	 * @return
+	 * 		The length of the byte array contained in the ID objects.
+	 */
+	public static synchronized int getIDLength() {
+		return idLengthInBytes;
+	}
+
+	/**
+	 * Changes the variable that regulates the length of ID byte arrays.
+	 * @param idLengthInBytes
+	 */
+	public static synchronized void setIDLength(int idLengthInBytes) 
 	{
-		// Return the id queue
-		return ID.idQueue;
+		ID.idLengthInBytes = idLengthInBytes;
 	}
-	
-	public static int getMaxQueueLength() 
+
+	/**
+	 * Basic getter.
+	 * @return
+	 * 		Returns the max queue length as determined by the maxQueueLength static variable.
+	 */
+	public static synchronized int getMaxQueueLength() 
 	{
-		// Return the max queue length
-		return ID.maxQueueLength;
+		return maxQueueLength;
 	}
 	
-	public static int getQueueLength()
+	/**
+	 * Basic setter.
+	 * @param maxQueueLength
+	 */
+	public static synchronized void setMaxQueueLength(int maxQueueLength) 
 	{
-		// Return the queue length
-		return ID.queueLength;
+		ID.maxQueueLength = maxQueueLength;
+	}
+
+	/**
+	 * Basic getter for the queue containing IDs.
+	 * @return
+	 * 		A SynchronizedLinkedListQueue containing a list of IDs.
+	 */
+	public static synchronized LinkedList getQueue() 
+	{
+		return idQueue;
+	}
+
+	/**
+	 * Basic getter.
+	 * @return
+	 * 		The current queue length. 
+	 */
+	public static int getQueueLength() {
+		return queueLength;
 	}
 	
+	/**
+	 * Basic getter.
+	 * @return
+	 * 		The zero id. 
+	 */
 	public static ID getZeroID()
 	{
-		// Return the zero ID
 		return ID.zeroID;
 	}
 	
-	public static void setIDLength(int lengthInBytes)
+	/**
+	 * Returns the internal byte representation of this ID.
+	 * @return
+	 * 		The byte array ID. 
+	 */
+	public byte[] getBytes()
 	{
-		// TODO check if >= 1
-		// Sets the id length
-		ID.idLengthInBytes = lengthInBytes;
-	}
-	
-	public static void setMaxQueueLength(int length)
-	{
-		// TODO will use removeAll
-		// TODO check parameter
-		// Sets the max queue length
-		ID.maxQueueLength = length;
-	}
-	
-	public byte[] getBytes() 
-	{
-		// TODO
 		return this.id.clone();
 	}
 	
-	@Override
-	public boolean equals(Object other)
-	{
-		boolean result = true;
-		
-		if(other == null)
-		{
-			result = false;
-		}
-		
-		if(!this.getClass().equals(this.getClass()))
-		{
-			result = false;
-		}
-		
-		if(this.id != ((ID) other).id)
-		{
-			result = false;
-		}
-		
-		// TODO
-		return result;
-	}
 
 	@Override
-	public int hashCode() 
+	/**
+	 * Checks to see if two ID objects have the same internal bytes. 
+	 */
+	public boolean equals(Object other)
 	{
-		// TODO the id?
-		// A = B and A != C, but A and C can have the same hash code.
-		// A and B MUST have the same hashCode.
-		return 0;
+		byte[] otherBytes;
+		byte[] thisBytes;
+		
+		thisBytes = this.id;
+		
+		try
+		{
+			otherBytes = ((ID)other).getBytes();
+		}catch(Exception e)
+		{
+			throw new IllegalArgumentException("Compared object is not of class ID");
+		}
+		
+		return Arrays.equals(otherBytes,thisBytes);
+		
 	}
 	
 	@Override
-	public String toString()
+	/**
+	 * Returns the hashCode of the toString method.
+	 */
+	public int hashCode()
 	{
-		// TODO return in hex
-		return null;
+		return this.toString().hashCode();
 	}
+	
+	@Override
+	/**
+	 * Converts each byte to hex, and returns the string consisting of consecutive hex characters.
+	 */
+	public String toString()
+	{ 
+		String toReturn;
 		
+		toReturn = "";
+		for(byte b: this.getBytes())
+		{
+			toReturn = toReturn + Integer.toHexString(b);
+		}
+		
+		return toReturn;
+	}
+	
 }
