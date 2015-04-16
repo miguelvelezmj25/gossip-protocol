685824787b2cb11dc5b62e2f71976fed9f44cffc
miguelvelemj25 <miguelvelez@mijecu25.com>
2015-04-16 14:54:05 -0500

    PortNumber

---
+++ src/PortNumber.java
@@ -0,0 +1,18 @@
+
+public class PortNumber {
+
+	private int portNumber;
+	
+	public PortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+	
+	public int get() {
+		return this.portNumber;
+	}
+	
+	public String toString() {
+		return ("" + this.portNumber);
+	}
+	
+}
