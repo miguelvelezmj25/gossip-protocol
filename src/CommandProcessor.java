bdc8b310579f60721c532ed7a06ee5a7cf8ad59d
Zaktius <pkmnbronze@gmail.com>
2015-04-18 16:25:23 -0500

    Create CommandProcessor.java

---
+++ src/CommandProcessor.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class CommandProcessor
+{
+	/*
+		Courtney Karppi
+		CISC 370
+		April 18, 2015
+	*/
+	private static 	Map<String,Command>commandRegistry;
+	private Command noSuchCommand;
+	private Command nothingEnteredCommand;
+
+	public CommandProcessor(Command noSuchCommand, Command nothingEnteredCommand)
+	{
+		this.noSuchCommand = noSuchCommand;
+		this.nothingEnteredCommand = nothingEnteredCommand;
+	}//CommandProcessor
+
+	//getAllCommands method returns all the commands in the commandRegistry
+	public Command[] getAllCommands()
+	{
+		//returns all the commands in the form of a Command[]
+		return commandRegistry.values().toArray(new Command[0]);
+	}//getAllCommands
+
+	//registers a new command in the commandRegistry
+	public void register(Command command)
+	{
+		String commandText;
+		commandText = convertToKey(command.getCommandName());
+
+		//checks the parameter to make sure it is not blank or not in the commandRegistry
+		if(!(commandText.equals("")||commandRegistry.containsKey(commandText)))
+		{
+			commandRegistry.put(commandText, command);
+		}
+	}//register
+
+	//runs the command associated with the parameter
+	public void runCommand(String commandText)
+	{
+		Command command;
+
+		//checks to see if the paramter is not blank or already in the commandRegistry
+		//if its not then it executes the associated command
+		if(!convertToKey(commandText).equals(""))
+		{
+			command = this.nothingEnteredCommand;
+		}
+		else if (!commandRegistry.containsKey(convertToKey(commandText)))
+		{
+			command = this.noSuchCommand;
+		}
+		else
+		{
+			command = commandRegistry.get(convertToKey(commandText));
+		}
+		command.execute();
+	}//runCommand
+
+	//Removes the white space from the data and converts it to lower case
+	private static String convertToKey(String data)
+	{
+		return data.trim().toLowerCase();
+	}//convertToKey
+
+}//class
