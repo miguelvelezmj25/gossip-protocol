a376dcc8d8331851fdf8ee482a5f25166b4073dc
rslechta <slec0016@stthomas.edu>
2015-04-18 16:14:08 -0500

    Update DatagramSender.java

--- src/DatagramSender.java
+++ src/DatagramSender.java
@@ -1,26 +1,96 @@
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class DatagramSender extends DatagramSenderReceiver {
+
+public abstract class DatagramSenderReceiver implements Runnable
+{
+	
+	private AtomicBoolean 				done;
+	private DatagramSocket 				datagramSocket;
+	private int							packetSize;
+	private SynchronizedLinkedListQueue queue;
+	
 	/**
-	 * 
+	 * Binds the datagramSocket to the inetSocketAddress, and instantiates the queue and packet size
+	 * instance variables. Also sets the atomic boolean variable to false.
 	 * @param inetSocketAddress
 	 * @param queue
 	 * @param packetSize
-	 * @throws SocketException 
+	 * @throws SocketException
 	 */
-	public DatagramSender(InetSocketAddress inetSocketAddress, OutgoingPacketQueue queue, int packetSize) throws SocketException 
-	{
-		// TODO
-		super(inetSocketAddress, queue, packetSize);
+	public DatagramSenderReceiver(InetSocketAddress inetSocketAddress, SynchronizedLinkedListQueue queue, int packetSize) throws SocketException 
+	{	
+		this.done.set(false);
+		this.packetSize = packetSize;
+		this.queue = queue;
+		datagramSocket = new DatagramSocket(inetSocketAddress);
 	}
-
-	@Override
-	public void action(DatagramSocket datagramSocket, SynchronizedLinkedListQueue queue) 
+	
+	/**
+	 * Basic getter.
+	 * @return
+	 * 		packet size
+	 */
+	public int getPacketSize()
 	{
-		// TODO 
-
+		return this.packetSize;
 	}
-
+	
+	/**
+	 * While the atomic boolean is not set to done, the method runs and performs the given action, sleeping
+	 * 100 miliseconds after every action.
+	 */
+	public void run()
+	{
+		while(!this.isStopped())
+		{
+			this.action(this.datagramSocket, this.queue);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Thread sleep method was interrupted.");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * Sets the atomic boolean to true, and closes the datagram socket.
+	 */
+	public void stop()
+	{
+		this.done.set(true);
+		this.datagramSocket.close();
+	}
+	
+	/**
+	 * Determines if the run method has been stopped.
+	 * @return
+	 * 		The get method on the atomic boolean.
+	 */
+	public boolean isStopped()
+	{
+		return this.done.get();
+	}
+	
+	/**
+	 * Starts a new thread and returns a reference to the thread.
+	 * @return
+	 * 		A reference to the newly started thread. 
+	 */
+	public Thread startAsThread() 
+	{
+		Thread thread;
+		
+		thread = new Thread(this);
+		thread.start();
+		
+		return thread;
+	}
+	
+	public abstract void action(DatagramSocket datagramSocket, SynchronizedLinkedListQueue queue);
+	
 }
