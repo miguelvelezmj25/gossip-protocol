52dfb53092a3f871767d8ebb2444b72d204ff506
rslechta <slec0016@stthomas.edu>
2015-04-18 16:13:46 -0500

    Update DatagramReceiver.java

--- src/DatagramReceiver.java
+++ src/DatagramReceiver.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
@@ -15,9 +17,38 @@ public class DatagramReceiver extends DatagramSenderReceiver {
 	}
 
 	@Override
+	/**
+	 * Listens at the datagram socket until a packet is received. When the a packet is received, the
+	 * method will immediately create a new datagram packet which will be placed in the passed queue. 
+	 * Once thats done, the thread sleeps for one hundred milliseconds. 
+	 */
 	public void action(DatagramSocket datagramSocket, SynchronizedLinkedListQueue queue)
 	{
-		// TODO 
+		DatagramPacket p;
+		
+		p = new DatagramPacket(new byte[super.getPacketSize()], super.getPacketSize());
+		while(!super.isStopped())
+		{
+			try 
+			{
+				datagramSocket.receive(p);
+				queue.enQueue(new DatagramPacket(p.getData(), super.getPacketSize()));
+				Thread.sleep(100);
+			} 
+			catch (IOException e) 
+			{
+				// TODO Auto-generated catch block
+				System.err.println("Shit man, IOException in the datagramSocket receive method. No idea what would cause this."); 
+				e.printStackTrace();
+			} 
+			catch (InterruptedException e) 
+			{
+				// TODO Auto-generated catch block
+				System.err.println("Shit man, your sleep method got interrupted.");
+				e.printStackTrace();
+			}
+			
+		}
 
 	}
 
