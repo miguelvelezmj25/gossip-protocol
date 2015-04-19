1829258914b8d0c99f359d258c86a4c9ae8221a2
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-19 11:38:29 -0500

    Comments about future change in class

--- src/PortNumberForSending.java
+++ src/PortNumberForSending.java
@@ -1,6 +1,8 @@
 /**
  * Use port 12345. Might not need this class since we are using the same port
  * for sending and receiving.
+ * 
+ * TODO this whole class is going to change.
  */
 public class PortNumberForSending extends PortNumber {
 
