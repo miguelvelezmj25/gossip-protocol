177738a298b5788c567d21c7fd334a7dce2bf0ac
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-19 11:46:39 -0500

    Initial commit of InvalidPacketFormatException

---
+++ src/InvalidPacketFormatException.java
@@ -0,0 +1,6 @@
+// TODO check with Jarvis about extending
+public class InvalidPacketFormatException extends RuntimeException {
+
+	private static final long serialVersionUID = 9184206436650524971L;
+
+}
