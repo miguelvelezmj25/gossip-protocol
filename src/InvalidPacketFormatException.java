038c2374640ce1313656e7737a78a24a6541161f
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-22 08:36:06 -0500

    Created constructor

--- src/InvalidPacketFormatException.java
+++ src/InvalidPacketFormatException.java
@@ -1,6 +1,13 @@
-// TODO check with Jarvis about extending
+import java.net.DatagramPacket;
+
+
 public class InvalidPacketFormatException extends RuntimeException {
 
 	private static final long serialVersionUID = 9184206436650524971L;
+	
+	public InvalidPacketFormatException(DatagramPacket packet, String errorMessage) {
+		// TODO what to do with the packet
+		super(errorMessage);
+	}
 
 }
