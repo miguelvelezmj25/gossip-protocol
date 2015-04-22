db377bdcae99b3f40dd94082aeb20e72cf21556e
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-22 18:43:43 -0500

    Commenting UDPMessage

--- src/UDPMessage.java
+++ src/UDPMessage.java
@@ -1,4 +1,8 @@
-
+/**
+ * This takes the packets from the outside world and transforms them into
+ * UDPMessages according to our protocol. This is where the InvalidPacket
+ * exception will be used.
+ */
 public class UDPMessage {
 
 }
