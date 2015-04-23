09074a3500ce06b91402d816088b3d58ac42a7ff
karp3138 <karp3138@stthomas.edu>
2015-04-23 14:55:41 -0500

    Update CommandProcessor.java

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-// TODO getCommand is missing
+// TODO Test
 public class CommandProcessor
 {
 	/*
@@ -8,26 +8,26 @@ public class CommandProcessor
 		CISC 370
 		April 18, 2015
 	*/
-	private static 	Map<String,Command>commandRegistry; // TODO static?
-	private Command noSuchCommand; 
+	private Map<String,Command>commandRegistry;
+	private Command noSuchCommand;
 	private Command nothingEnteredCommand;
 
 	public CommandProcessor(Command noSuchCommand, Command nothingEnteredCommand)
 	{
 		this.noSuchCommand = noSuchCommand;
 		this.nothingEnteredCommand = nothingEnteredCommand;
-		// TODO commandRegistry???
+		this.commandRegistry = new HashMap<String, Command>();
 	}//CommandProcessor
 
 	//getAllCommands method returns all the commands in the commandRegistry
-	public Command[] getAllCommands() // TODO test
+	public Command[] getAllCommands()
 	{
 		//returns all the commands in the form of a Command[]
 		return commandRegistry.values().toArray(new Command[0]);
 	}//getAllCommands
 
 	//registers a new command in the commandRegistry
-	public void register(Command command) // TODO test
+	public void register(Command command)
 	{
 		String commandText;
 		commandText = convertToKey(command.getCommandName());
@@ -40,26 +40,10 @@ public class CommandProcessor
 	}//register
 
 	//runs the command associated with the parameter
-	public void runCommand(String commandText) // TODO what is this?
+	public Command getCommand(String commandText)
 	{
-		Command command;
-
-		//checks to see if the paramter is not blank or already in the commandRegistry
-		//if its not then it executes the associated command
-		if(!convertToKey(commandText).equals(""))
-		{
-			command = this.nothingEnteredCommand;
-		}
-		else if (!commandRegistry.containsKey(convertToKey(commandText)))
-		{
-			command = this.noSuchCommand;
-		}
-		else
-		{
-			command = commandRegistry.get(convertToKey(commandText));
-		}
-		command.execute();
-	}//runCommand
+		return commandRegistry.get(convertToKey(commandText));
+	}//getCommand
 
 	//Removes the white space from the data and converts it to lower case
 	private static String convertToKey(String data)
