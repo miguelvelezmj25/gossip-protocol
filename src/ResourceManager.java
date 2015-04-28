87c4d66de0b6453e2ca9cce04229e6b31b325dca
Zaktius <pkmnbronze@gmail.com>
2015-04-28 16:52:11 -0500

    Implemented loadResourcesFrom and getResourcesThatMatch

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -1,5 +1,5 @@
-import java.io.File;
-import java.util.HashMap;
+import java.io.*;
+import java.util.*;
 
 
 public class ResourceManager
@@ -16,7 +16,13 @@ public class ResourceManager
 	
 	public static ResourceManager getInstance()
 	{
-		return resourcesManager;
+		ResourceManager result;
+		result = resourcesManager;
+		if(resourcesManager == null)
+		{
+			result = new ResourceManager();
+		}
+		return result;
 	}
 	
 	public static ResourceManager newInstance()
@@ -26,21 +32,65 @@ public class ResourceManager
 	
 	public synchronized void loadResourcesFrom(File file)
 	{
-		String description;
-		String location;
-		String mimeType;
 		ID resourceID;
-		String data;
+		Resource resource;
+		BufferedReader bf;
+		String line;
 		
-		location = file.getAbsolutePath();
-		
-		
-		
+		try
+		{
+			bf = new BufferedReader(new FileReader(file));
+			line = bf.readLine();
+			while(line != null)
+			{
+				resourceID = new ID(new byte[16]);
+				resource = new Resource(resourceID,line);
+				resourceDirectory.put(resourceID, resource);
+				
+				
+			}
+		}
+		catch(FileNotFoundException fnfe)
+		{
+			System.out.println(fnfe.getMessage());
+		}
+		catch(IOException ioe)
+		{
+			System.out.println(ioe.getMessage());
+		}
 		
 	}
 	public synchronized Resource[] getResourcesThatMatch(String searchString)
 	{
-		return null;
+		Object[] values;
+		int count;
+		Resource[] result;
+		
+		
+		values = resourceDirectory.values().toArray();
+		count = 0;
+		
+		for(int i = 0; i < values.length; i = i + 1)
+		{
+			if(((Resource)values[i]).matches(searchString))
+			{
+				count = count + 1;
+			}
+		}
+		
+		result = new Resource[count];
+		count = 0;
+		for(int i = 0; i < values.length; i = i + 1)
+		{
+			if(((Resource)values[i]).matches(searchString))
+			{
+				result[count] = (Resource)values[i];
+				count = count + 1;
+			}
+		}		
+		
+		
+		return result;
 	}
 	
 }
