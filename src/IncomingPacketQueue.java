c9e16fe0ebaf212ddda167b2ea2dd4539997ac80
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-28 19:31:59 -0500

    Adding comments

--- src/IncomingPacketQueue.java
+++ src/IncomingPacketQueue.java
@@ -1,4 +1,21 @@
 
-public class IncomingPacketQueue extends SynchronizedPacketQueue {
+public class IncomingPacketQueue extends SynchronizedPacketQueue 
+{
+	/**
+	 * 
+	 *	Miguel Velez
+	 * 	April 14, 2015
+	 * 
+	 * 	This class implements an incoming packet queue to catch compiler errors due to
+	 *  wrong data types. 
+	 * 
+	 * 	Modification History:
+	 * 		April 14, 2015
+	 * 			Original version
+	 * 
+	 * 	 	March 28, 2015
+	 * 			Added some comments.
+	 * 
+	 */
 
 }
--- src/OutgoingPacketQueue.java
+++ src/OutgoingPacketQueue.java
@@ -1,4 +1,22 @@
 
-public class OutgoingPacketQueue extends SynchronizedPacketQueue {
+public class OutgoingPacketQueue extends SynchronizedPacketQueue 
+{
+	/**
+	 * 
+	 *	Miguel Velez
+	 * 	April 14, 2015
+	 * 
+	 * 	This class implements an outgoing packet queue to catch compiler errors due to
+	 *  wrong data types. 
+	 * 
+	 * 	Modification History:
+	 * 		April 14, 2015
+	 * 			Original version
+	 * 
+	 * 	 	March 28, 2015
+	 * 			Added some comments.
+	 * 
+	 */
+
 	
 }
