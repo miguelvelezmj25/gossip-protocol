d09465c576f97776fc5f80474a5fb827a9673fa3
Miguel Velez <miguelvelez@mijecu25.com>
2015-04-28 21:41:26 -0500

    Commenting request

--- src/Request.java
+++ src/Request.java
@@ -1,31 +1,54 @@
 
-public abstract class Request {
-	
+public abstract class Request 
+{
+	/**
+	 * 
+	 * Miguel Velez
+	 * April 28, 2015
+	 * 
+	 * This singleton class manages the requests.
+	 * 
+	 * Class variables:
+	 * 
+	 * 		private ID requestID
+	 * 			the request id
+	 * 
+	 * Constructors:
+	 * 		
+	 * 		public Request(ID id) 
+	 * 			create a request
+	 * 
+	 * Methods:
+	 * 	
+	 * 		public ID getID() 
+	 * 			get the id from the request
+	 * 	
+	 * 		public abstract void updateRequest(UDPMessage udpMessage);
+	 *			update the request
+	 * 
+	 *      
+	 * Modification History:
+	 *  
+	 * 		April 28, 2015
+	 * 			Original version.
+	 *  
+	 */
+		
 	private ID requestID;
 	
-	/**
-	 * Create a request
-	 * 
-	 * @param id
-	 */
-	public Request(ID id) {
+	public Request(ID id) 
+	{
+		// Create a request
 		this.requestID = id;
 	}
-	
-	/**
-	 * Get the id from the request
-	 * 
-	 * @return
-	 */
-	public ID getID() {
+
+	public ID getID() 
+	{
+		// Get the id from the request
 		return this.requestID;
 	}
 	
-	/**
-	 * Update the request
-	 * 
-	 * @param udpMessage
-	 */
+	// Update the request
 	public abstract void updateRequest(UDPMessage udpMessage);
 
 }
