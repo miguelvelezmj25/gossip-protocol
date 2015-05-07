5435ab785006d3b2960e1b4f134ce77f81f3dd12
karp3138 <karp3138@stthomas.edu>
2015-05-07 10:19:54 -0500

    Update GossipPartners.java

--- src/GossipPartners.java
+++ src/GossipPartners.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class GossipPartners
 {
 	/*
@@ -5,27 +6,32 @@ public class GossipPartners
 	CISC 370 - GossipProtocol
 	May 5, 2015
 	*/
-	private static instance gossipPartners;
-	private Collection gossipPartners;
+
+	private static GossipPartners instance;
+	private Collection<GossipPartner> gossipPartners;
 
 	private GossipPartners()
 	{
-
+		gossipPartners = new Vector<GossipPartner>();
 	}//constructor
 
 	public static GossipPartners getInstance()
 	{
-
+		if(instance == null)
+		{
+			instance = new GossipPartners();
+		}
+		return instance;
 	}//getInstance
 
 	public static GossipPartners newInstance()
 	{
-
+		return getInstance();
 	}//newInstance
 
 	public void addPartner(GossipPartner gossipPartner)
 	{
-
+		gossipPartners.add(gossipPartner);
 	}//addPartner
 
 }//class
