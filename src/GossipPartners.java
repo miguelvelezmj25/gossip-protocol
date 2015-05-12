a0c36ea045939888a41210456540741a69474c92
karp3138 <karp3138@stthomas.edu>
2015-05-11 20:51:20 -0500

    Update GossipPartners.java

--- src/GossipPartners.java
+++ src/GossipPartners.java
@@ -5,8 +5,25 @@ public class GossipPartners
 	Courtney Karppi
 	CISC 370 - GossipProtocol
 	May 5, 2015
-	*/
 
+	Class Variables:
+		instance
+			An instnace of GossipPartners
+		gossipPartners
+			a collection of all the partners
+	Constructors:
+		GossipPartners()
+			creates a new vector for the instance variable
+	Methods:
+		public static GossipPartners getInstance()
+			If the instance is null it sets a new GossipPartners
+		public static GossipPartners newInstance()
+			Calls getInstance
+		public void addPartner(GossipPartner gossipPartner)
+			Adds a partner to the collection
+		public void send(UDPMessage udpMessage)
+			Sends the message to all the gossipPartners if the TTL is not zero
+	*/
 	private static GossipPartners instance;
 	private Collection<GossipPartner> gossipPartners;
 
@@ -17,6 +34,7 @@ public class GossipPartners
 
 	public static GossipPartners getInstance()
 	{
+		//If the instance is null it sets a new GossipPartners
 		if(instance == null)
 		{
 			instance = new GossipPartners();
@@ -26,24 +44,34 @@ public class GossipPartners
 
 	public static GossipPartners newInstance()
 	{
+		//Calls getInstance
 		return getInstance();
 	}//newInstance
 
 	public void addPartner(GossipPartner gossipPartner)
 	{
+		//Adds a partner to the collection
 		gossipPartners.add(gossipPartner);
 	}//addPartner
-	
+
 	public void send(UDPMessage udpMessage)
 	{
-		Iterator<GossipPartner> ie;
-		
-		ie = gossipPartners.iterator();
-		
-		while(ie.hasNext())
+		//Sends the message to all the gossipPartners if the TTL is not zero
+		if(udpMessage.getTimeToLive() == 0)
 		{
-			ie.next().send(udpMessage);
+			//doesn't send
 		}
-	}
+		else
+		{
+///HOW DO YOU MAKE A LOWER TTL FOR UDPMESSAGE
+			Iterator<GossipPartner> ie;
+			ie = gossipPartners.iterator();
+
+			while(ie.hasNext())
+			{
+				ie.next().send(udpMessage);
+			}
+		}
+	}//send
 
 }//class
