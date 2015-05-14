13daf73eeaa93577e1c51cdd2f0d5a265df5a372
Miguel Velez <miguelvelez@mijecu25.com>
2015-05-13 21:11:19 -0500

    Revert "Revert "Able to send and receive parts""
    This reverts commit 1b85d5d02333b756fa75f83644f5af0f0af8975b.

--- src/PeerController.java
+++ src/PeerController.java
@@ -1,12 +1,13 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-// TODO comment
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry.Entry;
 
 public class PeerController implements Runnable {
 	/**
@@ -84,16 +85,22 @@ public class PeerController implements Runnable {
 	 * 	 	May 11, 2015
 	 * 			Receiving packets from the community
 	 * 
+	 * 	 	May 11, 2015
+	 * 			Implemented sending and receiving a part
+	 * 
 	 */
 	
-	private AtomicBoolean			done;
-	private IncomingPacketQueue 	incomingPacketsFromCommunityQueue;
-	private IncomingPacketQueue 	incomingPacketsFromUIQueue;
-	private DatagramReceiver        receiveFromUI;
-	private OutgoingPacketQueue 	outgoingPacketsQueue;
-	private DatagramReceiver        receiveFromCommunity;
-	private DatagramSender			sender;
-	private InetSocketAddress		uiControllerAddress;
+	private AtomicBoolean					done;
+	private IncomingPacketQueue 			incomingPacketsFromCommunityQueue;
+	private IncomingPacketQueue 			incomingPacketsFromUIQueue;
+	private DatagramReceiver       			receiveFromUI;
+	private OutgoingPacketQueue 			outgoingPacketsQueue;
+	private DatagramReceiver        		receiveFromCommunity;
+	private HashMap<ID, ResourceRequester> 	resourceRequesters;
+	private DatagramSender					sender;
+	private InetSocketAddress				uiControllerAddress;
+	
+	// TODO might need a hashmap of partRequesters
 	
 	public PeerController(PortNumberPeerCommunity communityPort, PortNumberPeerUI uiPort, InetSocketAddress uiControllerAddress) 
 	{
@@ -112,18 +119,20 @@ public class PeerController implements Runnable {
 			
 			this.uiControllerAddress = uiControllerAddress;
 			
-			GossipPartners.getInstance().addPartner(new GossipPartner(new InetSocketAddress(InetAddress.getLocalHost(), 12345), this.outgoingPacketsQueue));
-//			GossipPartners.getInstance().addPartner(new GossipPartner(new InetSocketAddress(InetAddress.getByName("140.209.121.69"), 12345), this.outgoingPacketsQueue));
+			this.resourceRequesters = new HashMap<ID, ResourceRequester>();
+			
+			// Testing
+			GossipPartners.getInstance().addPartner(new GossipPartner(
+														new InetSocketAddress(
+																this.uiControllerAddress.getAddress(),
+																12345)
+															, this.outgoingPacketsQueue));
 
 		} 
 		catch (SocketException se) 
 		{
 			se.printStackTrace();
 		} 
-		catch (UnknownHostException ue)
-		{
-			ue.printStackTrace();
-		}
 	
 	}
 	
@@ -178,6 +187,17 @@ public class PeerController implements Runnable {
 		this.sender.stop();
 		this.receiveFromCommunity.stop();
 		
+//		Iterator<Map.Entry<ID,ResourceRequester>> requesterThreads = this.resourceRequesters.entrySet().iterator();
+//		
+//		while(requesterThreads.hasNext())
+//		{
+//			ResourceRequester requester = requesterThreads.next().getValue();
+//			
+//			requester.setDoneFlag(true);
+//			requester.notify();
+//		}
+		
+		
 	}
 
 	private void processCommandFromCommunity() {
@@ -190,7 +210,7 @@ public class PeerController implements Runnable {
 		UDPMessage communityMessage = new UDPMessage(communityPacket);
 		
 		// Pass the message to my peers
-		GossipPartners.getInstance().send(communityMessage);
+//		GossipPartners.getInstance().send(communityMessage);
 						
 		// Check if the ID2 matches is one of our responses
 		RequestFromUIController request = RequestManager.getInstance().getRequest(communityMessage.getID2()); 
@@ -199,9 +219,7 @@ public class PeerController implements Runnable {
 		Resource resource = ResourceManager.getInstance().getResourceFromID(communityMessage.getID2()); 
 		
 		if(request != null) 
-		{
-			// TODO almost done, just need to implement when getting a response for a get request
-			
+		{			
 			// Check if it is a find
 			if(request.getClass() == RequestFromUIControllerToFindResources.class)  
 			{
@@ -215,51 +233,70 @@ public class PeerController implements Runnable {
 			// Check if it is a find or a get
 			else if(request.getClass() == RequestFromUIControllerToGetaResource.class) 	
 			{
-				// TODO don't I have to just send the bytes to the UI? starting, end byte, length, bytes, part number?
-				
 				RequestFromUIControllerToGetaResource getRequest = (RequestFromUIControllerToGetaResource) request; 			
 				
-				System.out.println("get: " + getRequest.getID());
-				
+				// Update this request
+				getRequest.updateRequest(communityMessage);
 			}
 				
 		}
 		// Check if the ID2 matches one of our specific resources that the community wants
 		else if(resource != null) 
-		{			
+		{									
 			// Get the request part
 			byte[] partRequested = new byte[PartNumbers.getLengthInBytes()];
 		
 			System.arraycopy(communityMessage.getMessage(), ID.getLengthInBytes(), partRequested, 0, partRequested.length);
+			
+			// Get the integer representation of the part
+			int partNumberRequested = 0;
+		
+			// Get an int from a byte array
+			for(int i = 0; i < PartNumbers.getLengthInBytes(); i++) {
+				partNumberRequested = partNumberRequested | ((partRequested[i] & 0xFF) << ((PartNumbers.getLengthInBytes() - 1 - i) * 8));
+			}
 			
 			// Get a random ID
 			StringBuilder resourceResponse = new StringBuilder(new String(ID.idFactory().getBytes()));
 			
 			// Attach part requested
 			resourceResponse.append(new String(partRequested));
-			
-			// Get the actual integer value of the requested part
-			int partNumberRequested = Integer.parseInt(new String(partRequested).trim());
-			
+						
 			// Attache the bytes of the resource
 			resourceResponse.append(new String(resource.getBytesForPart(partNumberRequested)));
-
-			System.out.println(resource.getBytesForPart(partNumberRequested).length);
-			System.out.println(new String(resource.getBytesForPart(partNumberRequested)));
 			
 			// Create a message with format resourceID, requestID, TTL, randomId, part number, bytes. 
 			UDPMessage resourceMessage = new UDPMessage(communityMessage.getID2(), communityMessage.getID1(), new TimeToLive(), resourceResponse.toString());
 			
 			// Send to my peers
 			GossipPartners.getInstance().send(resourceMessage);
+			
+/////////////////// TODO delete testing
+////		System.out.println("id1: " + findMessage.getID1());
+////		System.out.println("id2: " + findMessage.getID2());
+////		System.out.println("tll: " + findMessage.getTimeToLive());
+////		System.out.println(new String(getMessage.getMessage()));
+//
+//			DatagramPacket send = resourceMessage.getDatagramPacket();
+//				
+//			send.setPort(12345);
+//			
+//			try {
+//				send.setAddress(InetAddress.getLocalHost());
+//	//			send.setAddress(InetAddress.getByName("140.209.121.69"));
+//			} catch (UnknownHostException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			outgoingPacketsQueue.enQueue(send);
+///////////////////// TODO delete	testing
+			
 		}
 		// Check if the text criteria matches something we might have
 		else if(ResourceManager.getInstance().getResourcesThatMatch(new String(communityMessage.getMessage())).length != 0)
 		{
 			// Get all the resources that match the criteria
 			Resource[] resources = ResourceManager.getInstance().getResourcesThatMatch(new String(communityMessage.getMessage()));
-			
-			System.out.println("Somebody is searching our files");
 			
 			// Process each resource
 			for(Resource ourResource : resources)
@@ -281,25 +318,25 @@ public class PeerController implements Runnable {
 				// Send to my peers
 				GossipPartners.getInstance().send(resourceMessage);	
 				
-////////////////////////////////////// TODO delete testing
-//				System.out.println("id1: " + resourceMessage.getID1().getBytes().length);
-//				System.out.println("id2: " + resourceMessage.getID2().getBytes().length);
-//				System.out.println("tll: " + resourceMessage.getTimeToLive());
-//				System.out.println("message of the file we just sent: " + new String(resourceMessage.getMessage()));
-	
-				DatagramPacket send = resourceMessage.getDatagramPacket();
-					
-				send.setPort(12345);
-				
-				try {
-					send.setAddress(InetAddress.getLocalHost());
-//					send.setAddress(InetAddress.getByName("140.209.121.69"));
-				} catch (UnknownHostException e) {
-					e.printStackTrace();
-				}
-				
-				this.outgoingPacketsQueue.enQueue(send);
-////////////////////////////////////// TODO delete	testing
+//////////////////////////////////////// TODO delete testing
+////				System.out.println("id1: " + resourceMessage.getID1().getBytes().length);
+////				System.out.println("id2: " + resourceMessage.getID2().getBytes().length);
+////				System.out.println("tll: " + resourceMessage.getTimeToLive());
+////				System.out.println("message of the file we just sent: " + new String(resourceMessage.getMessage()));
+//	
+//				DatagramPacket send = resourceMessage.getDatagramPacket();
+//					
+//				send.setPort(12345);
+//				
+//				try {
+//					send.setAddress(InetAddress.getLocalHost());
+////					send.setAddress(InetAddress.getByName("140.209.121.69"));
+//				} catch (UnknownHostException e) {
+//					e.printStackTrace();
+//				}
+//				
+//				this.outgoingPacketsQueue.enQueue(send);
+//////////////////////////////////////// TODO delete	testing
 				
 			}
 		}
@@ -338,78 +375,48 @@ public class PeerController implements Runnable {
 			// Send to the peer controllers
 			GossipPartners.getInstance().send(findMessage);
 			
-///////////////// TODO delete testing
-//			System.out.println("id1: " + findMessage.getID1());
-//			System.out.println("id2: " + findMessage.getID2());
-//			System.out.println("tll: " + findMessage.getTimeToLive());
-//			System.out.println(new String(findMessage.getMessage()));
-
-			DatagramPacket send = findMessage.getDatagramPacket();
-				
-			send.setPort(12345);
-			
-			try {
-				send.setAddress(InetAddress.getLocalHost());
-//				send.setAddress(InetAddress.getByName("140.209.121.69"));
-			} catch (UnknownHostException e) {
-				e.printStackTrace();
-			}
-			
-			this.outgoingPacketsQueue.enQueue(send);
-/////////////////// TODO delete	testing
+/////////////////// TODO delete testing
+////			System.out.println("id1: " + findMessage.getID1());
+////			System.out.println("id2: " + findMessage.getID2());
+////			System.out.println("tll: " + findMessage.getTimeToLive());
+////			System.out.println(new String(findMessage.getMessage()));
+//
+//			DatagramPacket send = findMessage.getDatagramPacket();
+//				
+//			send.setPort(12345);
+//			
+//			try {
+//				send.setAddress(InetAddress.getLocalHost());
+////				send.setAddress(InetAddress.getByName("140.209.121.69"));
+//			} catch (UnknownHostException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			this.outgoingPacketsQueue.enQueue(send);
+///////////////////// TODO delete	testing
 		}
 		// Check if it is a get request
 		else if(uiCommand.indexOf(delimiter + "get") == 0) 
 		{
-			// TODO still need to implement
-/////////////////// TODO all of this logic should be in a thread
-			// Create a get request id
-			ID getId = ID.idFactory();
-			
-			// create a get request
-			RequestFromUIControllerToGetaResource getRequest = new RequestFromUIControllerToGetaResource(getId, uiControllerAddress, this.outgoingPacketsQueue);
-			
-			// Save it in our request manager
-			RequestManager.getInstance().insertRequest(getRequest);
-			
 			// Get the resource id			
 			ID resourceID = RequestFromUIControllerToFindResources.getResource(Integer.parseInt(uiCommand.substring(5)));
-				
-			// Create a UDP message with format RequestID, ResourceID, TTL, RandomID, partNumber
+						
+			int partNumbers = (int) Math.ceil(ResourceManager.getInstance().getResourceFromID(resourceID).getSizeInBytes() / (double) 456);
+						
+			// Create and run the class that will request the resource
+			ResourceRequester resourceRequester = new ResourceRequester(resourceID, partNumbers);
 			
-			byte[] part = new byte[] {0,0,0,48};
-			
-			UDPMessage getMessage = new UDPMessage(getId, resourceID, new TimeToLive(), new String(ID.idFactory().getBytes()) + new String(part)); 
-			
-			GossipPartners.getInstance().send(getMessage);
-			
-//			System.out.println("id1: " + getMessage.getID1().getBytes().length);
-//			System.out.println("id2: " + getMessage.getID2().getBytes().length);
-//			System.out.println("tll: " + getMessage.getTimeToLive());
-//			System.out.println("message: " + new String(getMessage.getMessage()));
-//			System.out.println("resource: " + ResourceManager.getInstance().getResourceFromID(resourceID).getDescription());
-			
-/////////////////// TODO all of this logic should be in a thread
-			
-/////////////////// TODO delete
-//
-		DatagramPacket send = getMessage.getDatagramPacket();
-//		
-		send.setPort(12345);
-//		
-		try {
-			send.setAddress(InetAddress.getLocalHost());
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		}
-		
-		this.outgoingPacketsQueue.enQueue(send);
-/////////////////// TODO delete	
+//			// Save the requester in our collection
+//			this.resourceRequesters.put(resourceID, resourceRequester);
+
+			resourceRequester.startAsThread();
 		}
 		// The UI send an invalid command, send error back
 		else
 		{
 			System.out.println("UIController, you sent a bad request to the PeerController");
+			
+			// TODO leave this here in case we want to test later sending to UI
 //			String message = "UIController, you sent a bad request to the PeerController"; 
 //			byte[] buffer = new byte[message.getBytes().length];
 //			
@@ -456,10 +463,173 @@ public class PeerController implements Runnable {
 		// Check iff the peer is done processing packets
 		return this.done.get();
 	}
+		
 	
-	// TODO create a class to REQUEST A RESOURCE
-	// TODO Create a UDP message with format RequestID, ResourceID, TTL, RandomID, partNumber
-	// TODO how do we calculate how many parts to request? ceiling (length/ (double) 456)
 	
+	private class ResourceRequester implements Runnable {
+		/**
+		 * 
+		 * Miguel Velez
+		 * May 13, 2015
+		 * 
+		 * This class is in charge of requesting parts of resource
+		 * 
+		 * Class variables:
+		 * 
+		 * 		ID resourceID
+		 * 			the resource id we want to get
+		 * 
+		 * 		PartNumbers partNumbers
+		 * 			the part numbers that we need to get
+		 * 
+		 * 		AtomicBoolean done
+		 * 			Check if we are done processing packets
+		 * 
+		 *  
+	 	 * Constructors:
+	 	 * 
+	 	 * 		public ResourceRequester(ID resourceID, int partNumbers) 
+	 	 * 			create a resource requester
+	 	 * 
+	 	 *  
+		 * Methods:
+		 * 	
+		 * 		public void run() 
+		 * 			request one part at a time
+		 * 
+		 *  	public Thread startAsThread()
+		 *  		start this class as a thread
+		 *  	
+		 *  	public boolean isStopped()
+		 *  		check if done requesting all parts
+		 *  
+		 *  	public void setDoneFlag(boolean flag)
+		 *  		set the done flag
+		 *    	 
+		 *          
+		 * Modification History:
+		 * 		May 13, 2015
+		 * 			Original version
+		 *  
+		 */
 
+		private ID 				resourceID;
+		private PartNumbers 	partNumbers;
+		private AtomicBoolean	done;
+		
+		public ResourceRequester(ID resourceID, int partNumbers) 
+		{
+			this.resourceID = resourceID;
+			this.partNumbers = new PartNumbers(partNumbers);
+			this.done = new AtomicBoolean(false);
+		}
+		
+		@Override
+		public void run() {		
+			
+			while(!this.isStopped()) 
+			{
+				// Get all the part numbers
+//			for(int i = 0; i < this.partNumbers.get(); i++)
+				for(int i = 0; i < 1; i++)
+				{
+					// Get a byte array from the part number
+					byte[] partNumber = new byte[4];
+					
+					for(int j = 0; j < PartNumbers.getLengthInBytes(); j++) {
+						partNumber[j] = (byte) (i >> ((PartNumbers.getLengthInBytes() - 1 - j) * 8));
+					}
+					
+					// Create a get request id
+					ID getId = ID.idFactory();
+					
+					// create a get request
+					RequestFromUIControllerToGetaResource getRequest = new RequestFromUIControllerToGetaResource(getId, uiControllerAddress, outgoingPacketsQueue, this.partNumbers);
+					
+					// Save it in our request manager
+					RequestManager.getInstance().insertRequest(getRequest);
+					
+					// Create a UDP message with format RequestID, ResourceID, TTL, RandomID, partNumber			
+					UDPMessage getMessage = new UDPMessage(getId, resourceID, new TimeToLive(), new String(ID.idFactory().getBytes()) + new String(partNumber)); 
+					
+					// Send to peers
+					GossipPartners.getInstance().send(getMessage);	
+					
+					
+					
+/////////////////// TODO delete testing
+////			System.out.println("id1: " + findMessage.getID1());
+////			System.out.println("id2: " + findMessage.getID2());
+////			System.out.println("tll: " + findMessage.getTimeToLive());
+//				System.out.println(new String(getMessage.getMessage()));
+//
+//			DatagramPacket send = getMessage.getDatagramPacket();
+//				
+//			send.setPort(12345);
+//			
+//			try {
+//				send.setAddress(InetAddress.getLocalHost());
+////				send.setAddress(InetAddress.getByName("140.209.121.69"));
+//			} catch (UnknownHostException e) {
+//				e.printStackTrace();
+//			}
+//			
+//			outgoingPacketsQueue.enQueue(send);
+///////////////////// TODO delete	testing
+					
+//					System.out.println("yield");
+//					try {
+//						wait();
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+//					
+					try {
+						Thread.sleep(400);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+//					
+//					System.out.println("woke up");
+//					
+//					
+				}
+				
+				// stop this thread
+				this.done.set(true);
+				
+//				System.out.println("STOP REQUESTER");
+				
+			}
+
+			
+			
+		}
+		
+		public Thread startAsThread() {
+			// Start this class a a thread
+			Thread thread;
+			
+			thread = new Thread(this);
+			
+			// Execute the run method
+			thread.start();
+			
+			return thread;
+		}
+		
+		public boolean isStopped()
+		{
+			// Check iff the peer is done processing packets
+			return this.done.get();
+		}
+		
+		public void setDoneFlag(boolean flag)
+		{
+			// Setter for the done flag
+			this.done.set(flag);
+		}
+			
+	}
 }
--- src/RequestFromUIControllerToGetaResource.java
+++ src/RequestFromUIControllerToGetaResource.java
@@ -1,3 +1,4 @@
+import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 
@@ -12,7 +13,8 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 	 * 
 	 * Class variables:
 	 * 		
-	 * 		private ArrayList<ID> responses; 
+	 * 		boolean[] responses; 
+	 * 			the part numbers that correspond to responses of this request
 	 * 
 	 * Constructors:
 	 * 		
@@ -29,16 +31,21 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 	 *  
 	 * 		May 7, 2015
 	 * 			Original version.
+	 * 
+	 * 		May 13, 2015
+	 * 			Implemented getting a part number and sending to UI.
 	 *  
 	 */
 	
-	private ArrayList<ID> responses; 
+	private boolean[] responses; 
 	
-	public RequestFromUIControllerToGetaResource(ID id, InetSocketAddress uiController, OutgoingPacketQueue outgoingPacket) 
+	public RequestFromUIControllerToGetaResource(ID id, InetSocketAddress uiController, OutgoingPacketQueue outgoingPacket, PartNumbers numberOfParts) 
 	{
 		// Create a request to find resources from peers
 		// Call the super constructor
 		super(id, uiController, outgoingPacket);
+		
+		this.responses = new boolean[numberOfParts.get()];
 	}
 
 	@Override
@@ -51,7 +58,72 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 			throw new IllegalArgumentException("The UDP message that you provided is null");
 		}
 		
-		// TODO get the datagramPacket and send it to the UIController. Done in the peer controller
+		// Get the request part
+		byte[] partRequested = new byte[PartNumbers.getLengthInBytes()];
+	
+		System.arraycopy(udpMessage.getMessage(), ID.getLengthInBytes(), partRequested, 0, partRequested.length);
+				
+		// Get the integer representation of the part
+		int partNumberRequested = 0;
+	
+		// Get an int from a byte array
+		for(int i = 0; i < PartNumbers.getLengthInBytes(); i++) {
+			partNumberRequested = partNumberRequested | ((partRequested[i] & 0xFF) << ((PartNumbers.getLengthInBytes() - 1 - i) * 8));
+		}
+		
+		// Check if we have seen this response before
+		if(!this.responses[partNumberRequested]) 
+		{
+			// Add a response with the part number
+			this.responses[partNumberRequested] = true;
+
+			// Get the response packet
+			byte[] responseMessage = udpMessage.getMessage();
+			
+			// Get the bytes that we requested
+			byte[] bytesToSend = new byte[464];
+
+			// Get the starting byte
+			int startByte = partNumberRequested * (UDPMessage.getMaximumPacketSizeInBytes() - ID.getLengthInBytes() - PartNumbers.getLengthInBytes());
+
+			byte[] byteNumber = new byte[PartNumbers.getLengthInBytes()];
+			
+			for(int i = 0; i < PartNumbers.getLengthInBytes(); i++) {
+				byteNumber[i] = (byte) (startByte >> ((PartNumbers.getLengthInBytes() - 1 - i) * 8));
+			}
+			
+			// Put the 4 bytes of the starting byte at the beginning of the bytes to send
+			System.arraycopy(byteNumber, 0, bytesToSend, 0, PartNumbers.getLengthInBytes());
+						
+			// Get the end byte
+			int endByte = startByte + (UDPMessage.getMaximumPacketSizeInBytes() - ID.getLengthInBytes() - PartNumbers.getLengthInBytes());
+			
+			byteNumber = new byte[PartNumbers.getLengthInBytes()];
+			
+			for(int i = 0; i < PartNumbers.getLengthInBytes(); i++) {
+				byteNumber[i] = (byte) (endByte >> ((PartNumbers.getLengthInBytes() - 1 - i) * 8));
+			}
+			
+			// Put the 4 bytes of the end byte in spot 4 of the bytes to send
+			System.arraycopy(byteNumber, 0, bytesToSend, PartNumbers.getLengthInBytes(), PartNumbers.getLengthInBytes());
+						
+			// Copy the bytes to send
+			System.arraycopy(responseMessage, ID.getLengthInBytes() + PartNumbers.getLengthInBytes(), bytesToSend, (PartNumbers.getLengthInBytes() << 1), 456);
+			
+
+			// Create a new datagram
+			DatagramPacket resourceBytes = new DatagramPacket(bytesToSend, bytesToSend.length);
+						
+			// Set the address of the UICOntroller
+			resourceBytes.setAddress(this.getUIControllerAddress().getAddress());
+			
+			// Set the port of the UIController
+			resourceBytes.setPort(this.getUIControllerAddress().getPort());
+			
+			// Send the bytes as start, end, bytes
+			this.getQueue().enQueue(resourceBytes);
+		}
+		
 	}
 
 }
