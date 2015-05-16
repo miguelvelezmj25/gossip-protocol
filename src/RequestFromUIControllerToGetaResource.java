2f73722e73a3f84e8cf1f5e8cf326544cce5463a
Miguel Velez <miguelvelez@mijecu25.com>
2015-05-16 10:44:22 -0500

    Requested and received parts

--- src/RequestFromUIControllerToGetaResource.java
+++ src/RequestFromUIControllerToGetaResource.java
@@ -43,6 +43,9 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 	 * 
  	 * 		May 14, 2015
 	 * 			Using a thread to synchronize the sending and receiving.
+	 * 
+	 * 		May 16, 2015
+	 * 			Correctly requesting and receiving parts and sending it to UI.
 	 *  
 	 */
 		
@@ -58,96 +61,62 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 		
 		this.resourceID = resourceId;
 		this.responses = new boolean[numberOfParts];
-//		System.out.println(this.responses.length);
 	}
 
 	@Override
 	public void updateRequest(UDPMessage udpMessage) 
 	{
 		// Send the datagram packet to the UI controller
+		
 		// Check if null
 		if(udpMessage == null) 
 		{
 			throw new IllegalArgumentException("The UDP message that you provided is null");
 		}
 				
-		// Get the request part
+		// Get the request part which is an int
 		byte[] partRequested = new byte[PartNumbers.getLengthInBytes()];
 	
+		// Get the part 4 bytes for part which is 16 spots from the message. Put it in 4 bytes
 		System.arraycopy(udpMessage.getMessage(), ID.getLengthInBytes(), partRequested, 0, PartNumbers.getLengthInBytes());
-				
-//		System.out.println(partRequested[0] + " - " + partRequested[1] + " - " + partRequested[2] + " - " + partRequested[3]);
-		
+						
 		// Get the integer representation of the part
-		int partNumberRequested = ByteBuffer.wrap(partRequested).getInt();//0;
-	
-//		// Get an int from a byte array
-//		for(int i = 0; i < PartNumbers.getLengthInBytes(); i++) {
-//			partNumberRequested = partNumberRequested | ((partRequested[i] & 0xFF) << ((PartNumbers.getLengthInBytes() - 1 - i) * 8));
-//		}
-
-//		System.out.println("Part requested: " + partNumberRequested);
-//		
-//		System.out.println("Before synchronize");
-		
+		int partNumberRequested = ByteBuffer.wrap(partRequested).getInt();
+			
 		// Synchronize the responses array 
 		synchronized(this.responses) 
 		{
-//			System.out.println("Have we seen this part: " + partNumberRequested + " - " + this.responses[partNumberRequested]);
 			// Check if we have seen this response before
 			if(!this.responses[partNumberRequested]) 
-			{
-//				System.out.println("Part not seen before");
-				
+			{		
 				// Add a response with the part number
-				this.responses[partNumberRequested] = new Boolean(true);
+				this.responses[partNumberRequested] = true;
 				
-				// Get the response packet
-				byte[] responseMessage = udpMessage.getMessage();
 				
-				// Get the bytes that we requested
-				byte[] bytesToSend = new byte[488];
-				
-				// Pass the resource id
+				// Get the bytes that we requested. We are sending ID(16), start(8), end(8), bytes(456) 
+				byte[] bytesToSend = new byte[488]; // TODO check if we can use formulas
+								
+				// Pass the resource id(16) at the beginning of the packet that we are going to send
 				System.arraycopy(udpMessage.getID1().getBytes(), 0, bytesToSend, 0, ID.getLengthInBytes());
 				
-				// Get the starting byte
+				// Get the starting byte which is a multiple of 456
 				long startByte = partNumberRequested * (UDPMessage.getMaximumPacketSizeInBytes() - ID.getLengthInBytes() - (4));
-				
-//				System.out.println("start bytes: " + startByte);
-				
-				
+				// Create an array of 8 bytes to hold the long that represent the starting byte
 				byte[] byteNumber = ByteBuffer.allocate(8).putLong(startByte).array();
+				// Put the 8 bytes of the starting byte after the resourceID
+				System.arraycopy(byteNumber, 0, bytesToSend, ID.getLengthInBytes(), 8);			
 				
-//				System.out.println(byteNumber[0] + " - " + byteNumber[1] + " - " + byteNumber[2] + " - " + byteNumber[3] + " - " + byteNumber[4] + " - " + byteNumber[5] + " - " + byteNumber[6] + " - " + byteNumber[7]);
-				
-				// Put the 4 bytes of the starting byte at the 16 slot to send
-				System.arraycopy(byteNumber, 0, bytesToSend, ID.getLengthInBytes(), 8);
-				
-				
-				
-				
-				// Get the end byte
+				// Get the end byte which is 456 more bytes than the starting byte
 				long endByte = startByte + (UDPMessage.getMaximumPacketSizeInBytes() - ID.getLengthInBytes() - (4));
-				
-//				System.out.println("End bytes: " + endByte);
-				
+				// Create a new 8 byte array
 				byteNumber = ByteBuffer.allocate(8).putLong(endByte).array();
-				
-//				System.out.println(byteNumber[0] + " - " + byteNumber[1] + " - " + byteNumber[2] + " - " + byteNumber[3] + " - " + byteNumber[4] + " - " + byteNumber[5] + " - " + byteNumber[6] + " - " + byteNumber[7]);
-				
-				// Put the 4 bytes of the end byte in spot 20 of the bytes to send
+				// Put the 8 bytes of the end byte after the start byte array
 				System.arraycopy(byteNumber, 0, bytesToSend, (8) + ID.getLengthInBytes(), (8));
 				
+				// Copy the bytes to send after the end byte
+				System.arraycopy(udpMessage.getMessage(), ID.getLengthInBytes() + 4, bytesToSend, (16) + ID.getLengthInBytes(), 456);
 				
-				
-				// Copy the bytes to send
-				System.arraycopy(responseMessage, ID.getLengthInBytes() + 4, bytesToSend, (16) + ID.getLengthInBytes(), 456);
-				
-				
-//				System.out.println("Sending bytes to UI: " + bytesToSend.length);
-				
-				
+
 				// Create a new datagram
 				DatagramPacket resourceBytes = new DatagramPacket(bytesToSend, bytesToSend.length);
 				
@@ -162,9 +131,7 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 				
 				// Send the bytes as start, end, bytes
 				this.getQueue().enQueue(resourceBytes);
-				
-//				System.out.println("Sent to ui part:" + partNumberRequested);
-				
+								
 				// Notify to get a new part
 				this.responses.notify();
 			}
@@ -185,36 +152,29 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 				// Get a byte array from the part number
 				byte[] partNumber = new byte[4];
 				
-//				for(int j = 0; j < PartNumbers.getLengthInBytes(); j++) {
-//					partNumber[j] = (byte) (i >> ((PartNumbers.getLengthInBytes() - 1 - j) * 8));
-//				}
-				
+				// Get the current loop counter
 				int temp = i;
-//				
-//				System.out.println("\n" + i);
 								
+				// Transform an int to a byte array
 				for(int j = PartNumbers.getLengthInBytes() - 1; j >= 0; j--) {
-				
 					partNumber[j] = (byte) (temp & 0xFF);
-						temp = temp >>> 8;
-//					System.out.println(">>>> "+ (int)partNumber[j]);
+					temp = temp >>> 8;
 				}
-//				partNumber = ByteBuffer.allocate(4).putInt(i).array();
 				
-//				System.out.println(i+":"+ByteBuffer.wrap(partNumber).getInt());
+				// Create array to put random ID and part number
+				byte[] partRequested = new byte[ID.getLengthInBytes() + PartNumbers.getLengthInBytes()];
 				
-				byte[] message = new byte[ID.getLengthInBytes() + PartNumbers.getLengthInBytes()];
+				// Copy a random id in the array
+				System.arraycopy(ID.idFactory().getBytes(), 0, partRequested, 0, ID.getLengthInBytes());
 				
-//				new String(ID.idFactory().getBytes()) + new String(partNumber)
-//				
-				System.arraycopy(ID.idFactory().getBytes(), 0, message, 0, ID.getLengthInBytes());
-				System.arraycopy(partNumber, 0, message, ID.getLengthInBytes(), PartNumbers.getLengthInBytes());
+				// Copy the part number after the ID
+				System.arraycopy(partNumber, 0, partRequested, ID.getLengthInBytes(), PartNumbers.getLengthInBytes());
 				
 				// Create a UDP message with format RequestID, ResourceID, TTL, RandomID, partNumber			
-				UDPMessage getMessage = new UDPMessage(this.getID(), this.resourceID, new TimeToLive(), message); 
+				UDPMessage requestPartFromResourceMessage = new UDPMessage(this.getID(), this.resourceID, new TimeToLive(), partRequested); 
 				
 				// Send to peers
-				GossipPartners.getInstance().send(getMessage);	
+				GossipPartners.getInstance().send(requestPartFromResourceMessage);	
 								
 //				System.out.println("Requested part: " + i);
 
@@ -223,8 +183,19 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 				{
 					try 
 					{
-						// Wait
-						this.responses.wait(); 
+						// Wait until the thread is notified that the part request was recieved or 
+						// has passed 4 seconds
+						this.responses.wait(4000); 
+						
+						// If we waited 4 seconds and did not get a part
+						if(!this.responses[i]) {
+//							System.out.println("did not get part" + i);
+							// Decrement the loop counter to request the part again and break from
+							// the waiting
+							i--;
+							break;
+						}
+
 					} 
 					catch (InterruptedException ei) 
 					{
@@ -232,7 +203,7 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 					}			
 					
 				}
-						        
+									        
 	       	} 
 	       
 	    }
@@ -241,6 +212,7 @@ public class RequestFromUIControllerToGetaResource extends RequestFromUIControll
 	
 	public Thread startAsThread() 
 	{
+		// Start this class as a thread
 		Thread thread = new Thread();
 		
 		thread = new Thread(this);
