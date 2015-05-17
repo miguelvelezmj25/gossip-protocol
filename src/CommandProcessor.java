633ffbc4c7235a47d16b048de313c09ee975467f
karp3138 <karp3138@stthomas.edu>
2015-05-17 15:32:53 -0500

    Complete!

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 public class CommandProcessor
 {
 	/*
@@ -7,32 +6,49 @@ public class CommandProcessor
 		CISC 370
 		April 18, 2015
 
+		Class Variables:
+			commandRegistry
+				A map that contains all the commands and command name
+			noSuchCommand
+				A command that is used when the command doesn't exist
+			nothingEnteredCommand
+				A command that is used when nothing is entered
+		Constructors:
+			CommandProcessor(Command noSuchCommand, Command nothingEnteredCommand)
+				Sets the instance variable and creates a new hashmap
+		Methods:
+			public Command[] getAllCommands()
+				Returns all the commands in the commandRegistry in the form of a Command[]
+			public void register(Command command)
+				Registers a new command in the commandRegistry
+			public Command getCommand(String commandText)
+				Runs the command associated with the parameter
+			private static String convertToKey(String data)
+				Removes the white space from the data and converts it to lower case
 	*/
-	private Map<String,Command>commandRegistry;
-	private Command noSuchCommand;
-	private Command nothingEnteredCommand;
+	private Map<String,Command>		commandRegistry;
+	private Command 				noSuchCommand;
+	private Command 				nothingEnteredCommand;
 
 	public CommandProcessor(Command noSuchCommand, Command nothingEnteredCommand)
 	{
-		this.noSuchCommand = noSuchCommand;
-		this.nothingEnteredCommand = nothingEnteredCommand;
-		this.commandRegistry = new HashMap<String, Command>();
-
+		this.noSuchCommand			= noSuchCommand;
+		this.nothingEnteredCommand 	= nothingEnteredCommand;
+		this.commandRegistry 		= new HashMap<String, Command>();
 	}//CommandProcessor
 
-	//getAllCommands method returns all the commands in the commandRegistry
 	public Command[] getAllCommands()
 	{
-		//returns all the commands in the form of a Command[]
+		//Returns all the commands in the commandRegistry in the form of a Command[]
 		return commandRegistry.values().toArray(new Command[0]);
 	}//getAllCommands
 
-	//registers a new command in the commandRegistry
 	public void register(Command command)
 	{
-		String commandText;
-		commandText = convertToKey(command.getCommandName());
+		//Registers a new command in the commandRegistry
+		String 		commandText;
 
+		commandText = convertToKey(command.getCommandName());
 		//checks the parameter to make sure it is not blank or not in the commandRegistry
 		if(!(commandText.equals("")||commandRegistry.containsKey(commandText)))
 		{
@@ -40,10 +56,10 @@ public class CommandProcessor
 		}
 	}//register
 
-	//runs the command associated with the parameter
 	public Command getCommand(String commandText)
 	{
-		Command command;
+		//Runs the command associated with the parameter
+		Command 	command;
 		if(commandText.length() == 0)
 		{
 			command = this.noSuchCommand;
@@ -57,14 +73,12 @@ public class CommandProcessor
 			command = this.nothingEnteredCommand;
 		}
 		return command;
-
 	}//getCommand
 
-	//Removes the white space from the data and converts it to lower case
 	private static String convertToKey(String data)
 	{
+		//Removes the white space from the data and converts it to lower case
 		return data.trim().toLowerCase();
 	}//convertToKey
 
 }//class
-
